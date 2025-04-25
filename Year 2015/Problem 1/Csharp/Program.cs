4e6f6990bdcdafba9ba947d502e4166689998f94
William-M3 <WilliamTruMurphy@gmail.com>
2025-04-25 18:02:28 -0400

    Problem 1 2015 Part 1 and 2 in C#

---
+++ Year 2015/Problem 1/Csharp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+
+// Reading text file
+string? line;
+string puzzle = "";
+
+try
+{
+    StreamReader sr = new StreamReader("../../../../Puzzle.txt");
+    line = sr.ReadLine();
+    
+    while (line != null)
+    {
+        puzzle = line;
+        line = sr.ReadLine();
+    }
+
+    sr.Close();
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Message: {e.Message}");
+}
+
+char[] floorCommands = puzzle.ToCharArray();
+int floor = 0;
+
+// Part 1
+// foreach (char command in floorCommands)
+// {
+//     if (command == '(')
+//     {
+//         floor++;
+//     }
+//     else
+//     {
+//         floor--;
+//     }
+// }
+// Console.WriteLine(floor);
+
+int position = 0;
+
+// Part 2
+foreach (char command in floorCommands)
+{
+    if (command == '(')
+        floor++;
+    else
+        floor--;
+    position++;
+
+    if (floor == -1)
+    {
+        Console.WriteLine(position);
+        break;
+    }
+}
