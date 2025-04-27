c34f7788bf0fd221fbcdb18356f21b4b78283afa
William-M3 <WilliamTruMurphy@gmail.com>
2025-04-27 19:22:07 -0400

    Part 2 of Problem 2 2015 Solved in Python

--- Year 2015/Problem 2/Problem 2.py
+++ Year 2015/Problem 2/Problem 2.py
@@ -6,22 +6,49 @@ with open("Puzzle.txt") as file:
 #  Format of item in list
 # '20x3x11\n'
 
-wrapping_paper_needed = 0
+#wrapping_paper_needed = 0
 dimension_array = list()
 
 # Part 1
-for dimension in list_of_box_sizes:
+# for dimension in list_of_box_sizes:
     
-    # Clean Data
+#     # Clean Data
+#     dimension.removesuffix('\n')
+#     dimension_array = list()
+#     for x in dimension.split('x'):
+#         dimension_array.append(int(x))
+    
+#     # Finding Surface Area
+#     wrapping_paper_needed += 2 * dimension_array[0] * dimension_array[1] + 2 * dimension_array[1] * dimension_array[2] + 2 * dimension_array[0] * dimension_array[2]
+
+#     # Finding Slack 
+#     for i in range(len(dimension_array)):
+#         for j in range(i+1, len(dimension_array)):
+#             if (dimension_array[i] > dimension_array[j]):
+#                 temp = dimension_array[j]
+#                 dimension_array[j] = dimension_array[i]
+#                 dimension_array[i] = temp
+
+#     wrapping_paper_needed += dimension_array[0]* dimension_array[1]
+
+# print(wrapping_paper_needed)
+
+ribbon = 0
+
+# Part 2
+
+for dimension in list_of_box_sizes:
+
+    #Clean Data
     dimension.removesuffix('\n')
     dimension_array = list()
     for x in dimension.split('x'):
         dimension_array.append(int(x))
-    
-    # Finding Surface Area
-    wrapping_paper_needed += 2 * dimension_array[0] * dimension_array[1] + 2 * dimension_array[1] * dimension_array[2] + 2 * dimension_array[0] * dimension_array[2]
 
-    # Finding Slack 
+    #Bow is
+    ribbon += dimension_array[0] * dimension_array[1] * dimension_array[2]
+
+    #Smallest perimeter area
     for i in range(len(dimension_array)):
         for j in range(i+1, len(dimension_array)):
             if (dimension_array[i] > dimension_array[j]):
@@ -29,6 +56,6 @@ for dimension in list_of_box_sizes:
                 dimension_array[j] = dimension_array[i]
                 dimension_array[i] = temp
 
-    wrapping_paper_needed += dimension_array[0]* dimension_array[1]
+    ribbon += 2 * dimension_array[0] + 2 * dimension_array[1]
 
-print(wrapping_paper_needed)
+print(ribbon)
